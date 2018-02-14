a377087ffcf285ae785dc3cd2639a48044a9e04e
Pat Cavit <github@patcavit.com>
2018-02-13 23:58:15 -0800

    chore: prep for publishing

--- rollup.config.js
+++ rollup.config.js
@@ -1,26 +1,56 @@
-module.exports = {
+const plugins = [
+    require("rollup-plugin-node-resolve")(),
+    require("rollup-plugin-commonjs")(),
+
+    process.env.PRODUCTION ?
+        require("rollup-plugin-strip-code")({
+            start_comment : "START.TESTSONLY",
+            end_comment   : "END.TESTSONLY"
+        }) :
+        {},
+    
+    process.env.PRODUCTION ?
+        require("rollup-plugin-uglify")() :
+        {},
+];
+
+const es6 = {
     input : "src/index.js",
 
     output : [
         {
-            file : "dist/aws-sig.es.js",
-            format : "es"
+            file : "dist/aws-sig.esm.js",
+            format : "es",
+            sourcemap : true,
         },
         {
             file : "dist/aws-sig.cjs.js",
-            format : "cjs"
+            format : "cjs",
+            sourcemap : true,
         }
     ],
 
-    plugins : [
-        require("rollup-plugin-node-resolve")(),
-        require("rollup-plugin-commonjs")(),
-
-        process.env.PRODUCTION ?
-            require("rollup-plugin-strip-code")({
-                start_comment : "START.TESTSONLY",
-                end_comment   : "END.TESTSONLY"
-            }) :
-            {}
-    ]
+    plugins
 };
+
+const es5 = {
+    input : "src/index.js",
+
+    output : [
+        {
+            file : "dist/es5/aws-sig.esm.js",
+            format : "es",
+            sourcemap : true,
+        },
+        {
+            file : "dist/es5/aws-sig.cjs.js",
+            format : "cjs",
+            sourcemap : true,
+        }
+    ],
+
+    plugins : plugins.concat(require("rollup-plugin-buble")())
+};
+
+module.exports = process.env.PRODUCTION ?
+    [ es5, es6 ] : es6;
