c82597c14413144962ca5f55f0546a8537564230
Pat Cavit <github@patcavit.com>
2018-02-14 12:26:03 -0800

    chore: better release prep setup
    Thanks, rollup/rollup-starter-lib

--- rollup.config.js
+++ rollup.config.js
@@ -1,46 +1,52 @@
-const plugins = [
-    require("rollup-plugin-node-resolve")(),
-    require("rollup-plugin-commonjs")(),
+"use strict";
 
-    process.env.PRODUCTION ?
-        require("rollup-plugin-strip-code")({
-            start_comment : "START.TESTSONLY",
-            end_comment   : "END.TESTSONLY"
-        }) :
-        {},
-    
-    process.env.PRODUCTION ?
-        require("rollup-plugin-uglify")() :
-        {},
+const conditional = require("rollup-plugin-conditional");
+
+const pkg = require("./package.json");
+
+const input = "./src/index.js";
+const production = Boolean(process.env.PRODUCTION);
+
+module.exports = [
+    // browser UMD build
+    {
+        input,
+
+        output : {
+            name : pkg.name,
+            file : pkg.browser,
+            format : "umd"
+        },
+
+        plugins : [
+            require("rollup-plugin-node-resolve")(),
+            require("rollup-plugin-commonjs")(),
+            
+            conditional(production, [
+                require("rollup-plugin-buble")(),
+                require("rollup-plugin-strip-code")({
+                    start_comment : "START.TESTSONLY",
+                    end_comment   : "END.TESTSONLY"
+                }),
+                require("rollup-plugin-uglify")()
+            ])
+        ]
+    },
+
+    // ESM & CJS builds
+    {
+        input,
+
+        output : [{
+            file : pkg.main,
+            format : "cjs"
+        }, {
+            file : pkg.module,
+            format : "es"
+        }],
+
+        external : [
+            "strict-uri-encode"
+        ]
+    }
 ];
-
-const es6 = {
-    input : "src/index.js",
-
-    output : [
-        {
-            file : "dist/index.js",
-            format : "cjs",
-            sourcemap : true,
-        }
-    ],
-
-    plugins
-};
-
-const es5 = {
-    input : "src/index.js",
-
-    output : [
-        {
-            file : "dist/es5/index.js",
-            format : "cjs",
-            sourcemap : true,
-        }
-    ],
-
-    plugins : plugins.concat(require("rollup-plugin-buble")())
-};
-
-module.exports = process.env.PRODUCTION ?
-    [ es5, es6 ] : es6;
