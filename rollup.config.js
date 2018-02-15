98b44217c8a2ad96d882986979baf9d8fef77092
Pat Cavit <github@patcavit.com>
2018-02-14 23:09:16 -0800

    fix: always build UMD in production mode
    Tests don't use it anyways, and `--environment` settings weren't making it through for some reason. Takes a bit longer but not too bad.

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,8 @@
 "use strict";
 
-const conditional = require("rollup-plugin-conditional");
-
 const pkg = require("./package.json");
 
 const input = "./src/index.js";
-const production = Boolean(process.env.PRODUCTION);
 
 module.exports = [
     // browser UMD build
@@ -22,15 +19,12 @@ module.exports = [
         plugins : [
             require("rollup-plugin-node-resolve")(),
             require("rollup-plugin-commonjs")(),
-            
-            conditional(production, [
-                require("rollup-plugin-buble")(),
-                require("rollup-plugin-strip-code")({
-                    start_comment : "START.TESTSONLY",
-                    end_comment   : "END.TESTSONLY"
-                }),
-                require("rollup-plugin-uglify")()
-            ])
+            require("rollup-plugin-buble")(),
+            require("rollup-plugin-strip-code")({
+                start_comment : "START.TESTSONLY",
+                end_comment   : "END.TESTSONLY"
+            }),
+            require("rollup-plugin-uglify")()
         ]
     },
 
