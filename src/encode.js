233577905d150174769c0ae11e8b50b3a6546fe3
Pat Cavit <github@patcavit.com>
2019-08-17 23:47:19 -0700

    WIP: cjs => esm

--- src/encode.js
+++ src/encode.js
@@ -2,10 +2,11 @@
 
 // Replace one extra character beyond what encodeURIComponent does, "*"
 // See https://github.com/aws/aws-sdk-js/blob/38bf84c144281f696768e8c64500f2847fe6f298/lib/util.js#L39-L49
-module.exports = (str) =>
+const encode = (str) =>
     encodeURIComponent(str)
     .replace(/[*]/g, (x) =>
         // eslint-disable-next-line newline-per-chained-call
         `%${x.charCodeAt(0).toString(16).toUpperCase()}`
     );
 
+export default encode;
