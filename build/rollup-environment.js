02ec9f78b982bca3c8a9e96a6004fe0d66b80fd6
Pat Cavit <github@patcavit.com>
2019-08-18 22:46:51 -0700

    WIP: touch up the check

--- build/rollup-environment.js
+++ build/rollup-environment.js
@@ -3,6 +3,5 @@
 const plugin = require("rollup-plugin-consts");
 
 module.exports = plugin({
-    // eslint-disable-next-line
-    testing : process.env["ISTESTING"],
+    testing : Boolean(process.env.ISTESTING),
 });
