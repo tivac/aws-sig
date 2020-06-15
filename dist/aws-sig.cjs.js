a1c7cf431683b244c8bbddcf38732ca45920176a
Pat Cavit <github@patcavit.com>
2020-06-15 00:58:37 -0700

    release: 2.1.0 :tada: :rocket:

--- dist/aws-sig.cjs.js
+++ dist/aws-sig.cjs.js
@@ -1,14 +1,15 @@
-/*! aws-sig@2.0.1 !*/
+/*! aws-sig@2.1.0 !*/
 'use strict';
 
 Object.defineProperty(exports, '__esModule', { value: true });
 
-require('./sign.cjs.js');
+var sign = require('./sign.cjs.js');
 var signedHeaders = require('./signed-headers.cjs.js');
 var signedQuery = require('./signed-query.cjs.js');
 
 
 
+exports.headerXAmzDate = sign.xamzdate;
 exports.signedHeaders = signedHeaders.signedHeaders;
 exports.signedQuery = signedQuery.signedQuery;
 //# sourceMappingURL=aws-sig.cjs.js.map
